9337b64af3da2051304381a0b231868e57fbf654
lance.c.walton <lance.c.walton@94601f48-cfae-11dd-a8b6-f5aca7b582bb>
2011-04-13 20:43:33 +0000

    Handle arrays

--- com.stateofflow.eclipse.tane/src/java/com/stateofflow/eclipse/tane/unifydeclarations/UnifyDeclarationsRefactoring.java
+++ com.stateofflow.eclipse.tane/src/java/com/stateofflow/eclipse/tane/unifydeclarations/UnifyDeclarationsRefactoring.java
@@ -7,6 +7,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ArrayCreation;
+import org.eclipse.jdt.core.dom.ArrayInitializer;
+import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.Expression;
@@ -101,7 +104,7 @@ public class UnifyDeclarationsRefactoring extends Refactoring {
     private void replaceDeclarationWithAssignment(ASTRewrite rewrite, TextEditGroup editGroup, VariableDeclarationFragment fragment) {
         Assignment assignment = getAST().newAssignment();
         assignment.setLeftHandSide((SimpleName) rewrite.createCopyTarget(fragment.getName()));
-        assignment.setRightHandSide((Expression) rewrite.createCopyTarget(fragment.getInitializer()));
+        assignment.setRightHandSide(createExpressionToAssign(rewrite, fragment));
         VariableDeclarationStatement declaration = (VariableDeclarationStatement) fragment.getParent();
         if (declaration.fragments().size() == 1) {
             rewrite.remove(declaration, editGroup);
@@ -109,6 +112,20 @@ public class UnifyDeclarationsRefactoring extends Refactoring {
             rewrite.remove(fragment, editGroup);
         }
         rewrite.getListRewrite(declaration.getParent(), Block.STATEMENTS_PROPERTY).insertBefore(getAST().newExpressionStatement(assignment), declaration, editGroup);
+    }
+
+    public Expression createExpressionToAssign(ASTRewrite rewrite, VariableDeclarationFragment fragment) {
+        return isArray(fragment) && fragment.getInitializer().getNodeType() != ASTNode.ARRAY_CREATION ? createArrayCreationFromArrayInitializer(rewrite, fragment) : (Expression) rewrite.createCopyTarget(fragment.getInitializer());
+    }
+
+    public Expression createArrayCreationFromArrayInitializer(ASTRewrite rewrite, VariableDeclarationFragment fragment) {
+        VariableDeclarationStatement statement = (VariableDeclarationStatement) fragment.getParent();
+        Type elementType = (Type) rewrite.createCopyTarget(statement.getType().getNodeType() == ASTNode.ARRAY_TYPE ? ((ArrayType) statement.getType()).getElementType() : statement.getType());
+        ArrayType type = getAST().newArrayType(elementType, (statement.getType().getNodeType() == ASTNode.ARRAY_TYPE ? ((ArrayType) statement.getType()).getDimensions() : 0) + fragment.getExtraDimensions());
+        ArrayCreation creation = getAST().newArrayCreation();
+        creation.setType(type);
+        creation.setInitializer((ArrayInitializer) rewrite.createCopyTarget(fragment.getInitializer()));
+        return creation;
     }
 
     public void removeDeclaration(ASTRewrite rewrite, TextEditGroup editGroup, VariableDeclarationFragment fragment) {
@@ -134,6 +151,10 @@ public class UnifyDeclarationsRefactoring extends Refactoring {
         newDeclaration.setType((Type) rewrite.createCopyTarget(declarationStatement.getType()));
         
         rewrite.getListRewrite(block, Block.STATEMENTS_PROPERTY).insertBefore(newDeclaration, node, editGroup);
+    }
+    
+    private boolean isArray(VariableDeclarationFragment fragment) {
+        return fragment.getExtraDimensions() > 0 || ((VariableDeclarationStatement) fragment.getParent()).getType().isArrayType();
     }
 
     private AST getAST() {
